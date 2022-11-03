80b4caf17c5c7beea47adffa4e0f2cf2ad9ba2ab
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-11-02 17:35:35 -0700

    Working to fix spotless format checks

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,19 +4,34 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
     /** Creates a new ExampleSubsystem. */
-    public ExampleSubsystem() {}
+    public ExampleSubsystem() {
+        if (RobotBase.isReal()) {
+            realInit();
+        } else {
+            simInit();
+        }
+    }
+
+    private void realInit() {
+
+    }
+
+    private void simInit() {
+
+    }
 
     @Override
     public void periodic() {
-    // This method will be called once per scheduler run
+
     }
 
     @Override
     public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+
     }
 }
