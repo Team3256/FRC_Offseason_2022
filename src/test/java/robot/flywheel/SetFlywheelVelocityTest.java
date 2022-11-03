097b1080a0036d1adf6f464ee2e752ce025ca419
vivek nadig <vivek.nadig@warriorlife.net>
2022-11-03 10:22:25 -0700

    Removed errors in flywheel test file

--- src/test/java/robot/flywheel/SetFlywheelVelocityTest.java
+++ src/test/java/robot/flywheel/SetFlywheelVelocityTest.java
@@ -1,17 +1,12 @@
-import static org.junit.Assert.*;
+package robot.flywheel;
 
+import static org.junit.Assert.*;
 import edu.wpi.first.hal.HAL;
-//import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.simulation.DoubleSolenoidSim;
-import edu.wpi.first.wpilibj.simulation.PWMSim;
-import frc.robot.Constants.IntakeConstants;
 import org.junit.*;
 
 public class SetFlywheelVelocityTest {
     public static final double DELTA = 1e-2; // acceptable deviation range
-    Intake intake;
-    PWMSim simMotor;
-    DoubleSolenoidSim simPiston;
+
 
     @Before
     public void setup() {
