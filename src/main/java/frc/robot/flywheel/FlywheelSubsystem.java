0d9ef82513e84eb414e8da96ad26cffdb2a7e64a
vivek nadig <vivek.nadig@warriorlife.net>
2022-11-04 12:22:19 -0700

    Added test to SWA03

--- src/main/java/frc/robot/flywheel/FlywheelSubsystem.java
+++ src/main/java/frc/robot/flywheel/FlywheelSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.flywheel;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.FlywheelConstants;
 
--- src/test/java/robot/flywheel/SetFlywheelVelocityTest.java
+++ src/test/java/robot/flywheel/SetFlywheelVelocityTest.java
@@ -3,8 +3,6 @@ package robot.flywheel;
 import static org.junit.Assert.*;
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.flywheel.FlywheelSubsystem;
 import frc.robot.flywheel.commands.SetFlywheelVelocity;
 import frc.robot.flywheel.commands.StopFlywheel;
