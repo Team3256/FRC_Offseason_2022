0ceffa3fda2e16cd5ec0d88f250268fd97f772a4
WilliamT424 <w.tang@warriorlife.net>
2022-11-04 17:01:28 -0700

    William practice coding

--- src/main/java/frc/robot/flywheel/FlywheelSubsystem.java
+++ src/main/java/frc/robot/flywheel/FlywheelSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.flywheel;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.FlywheelConstants;
 
@@ -13,34 +15,40 @@ public class FlywheelSubsystem extends SubsystemBase {
      * 1 Follower TalonFX (Right)
      * Total: 2 TalonFX motors
      */
-
+    TalonFX adi;
+    TalonFX acky;
     public FlywheelSubsystem() {
-    }
+        adi = new TalonFX(FlywheelConstants.LEFT_CAN_ID);
+        acky = new TalonFX(FlywheelConstants.RIGHT_CAN_ID);
 
+        acky.follow(adi);
+    }
     /*
      * Stop the flywheel from moving
      */
     public void stopFlywheel() {
+         adi.neutralOutput();
     }
 
     /*
      * Set the speed of the motor using the Percent ControlMode
      */
     public void setPercentSpeed(double percent) {
+        adi.set(ControlMode.PercentOutput , percent);
     }
 
     /*
      * Get the percent speed of the master motor
      */
     public double getPercentSpeed() {
-        return 0.0;
+        return adi.getMotorOutputPercent();
     }
 
     /*
      * Get the percent speed of the following motor
      */
     public double getFollowingPercentSpeed() {
-        return 0.0;
+        return acky.getMotorOutputPercent();
     }
 
 
