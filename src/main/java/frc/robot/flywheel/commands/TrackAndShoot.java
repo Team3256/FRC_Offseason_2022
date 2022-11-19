b897dd9623cd18039fc99fda8b06f34ca1db77d5
3LucasZ <lucas.j.zheng@gmail.com>
2022-11-19 00:41:18 -0800

    Changed limelight and new command group

--- src/main/java/frc/robot/flywheel/commands/TrackAndShoot.java
+++ src/main/java/frc/robot/flywheel/commands/TrackAndShoot.java
@@ -1,19 +1,24 @@
 package frc.robot.flywheel.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import frc.robot.flywheel.FlywheelSubsystem;
 import frc.robot.hardware.Limelight;
 import frc.robot.turret.TurretSubsystem;
 import frc.robot.turret.commands.SetTurretPosition;
 
+import java.util.function.DoubleSupplier;
+
 
 // TODO: Make this extend a command grouper and finish this class
-public class TrackAndShoot extends /* Command group here */ {
-    FlywheelSubsystem flywheelSubsystem;
-    TurretSubsystem turretSubsystem;
-
+public class TrackAndShoot extends ParallelCommandGroup {
     // Run both the SetFlywheelVelocity command and SetTurretPosition commands 
-    public TrackAndShoot() {
+    public TrackAndShoot(FlywheelSubsystem flywheelSubsystem, TurretSubsystem turretSubsystem) {
+        addCommands(
+                //set flywheel velocity
+                new SetFlywheelVelocityFromPID(flywheelSubsystem, Limelight.getTy()),
+                //set turret position
+                new SetTurretPosition(turretSubsystem, Limelight.getTx()));
     }
 }
